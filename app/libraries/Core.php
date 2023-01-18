14bebe7506e3cf621629da18eddbb3aba5c37277
LAMNAZZAH <medlmz01@gmail.com>
2023-01-18 18:53:19 +0100

    [add] Load the controller

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -7,10 +7,23 @@
 
         public function __construct()
         {
-            $this->getUrl();
+            $url = $this->getUrl();
+
+            if($url && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+                $this->currentController = ucwords($url[0]);
+                unset($url[0]);
+            }
+
+            require_once '../app/controllers/' . $this->currentController . '.php';
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl() {
-            echo $_GET['url'];
+            if(isset($_GET['url'])) {
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
         }
     }
