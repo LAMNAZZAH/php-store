3020640344629a2b950398afd4c2e8ba61e1c858
LAMNAZZAH <medlmz01@gmail.com>
2023-02-01 19:17:33 +0100

    minor: navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -5,15 +5,27 @@
     <nav class="navbar">
         <ul>
             <li>
-            <img class="nav-icon" src=<?php echo URLROOT . "/public/img/store.svg" ?> alt="">    
-            Explore</li>
+                <img class="nav-icon" src=<?php echo URLROOT . "/public/img/store.svg" ?> alt="">
+                Explore
+            </li>
             <li>
-            <img class="nav-icon" src=<?php echo URLROOT . "/public/img/cart.svg" ?> alt="">     
-            My Cart</li>
+                <img class="nav-icon" src=<?php echo URLROOT . "/public/img/cart.svg" ?> alt="">
+                My Cart
+            </li>
             <li>
-            <img class="nav-icon" src=<?php echo URLROOT . "/public/img/about.svg" ?> alt="">     
-            About</li>
+                <img class="nav-icon" src=<?php echo URLROOT . "/public/img/about.svg" ?> alt="">
+                About
+            </li>
         </ul>
+        <div class="search-block">
+            <div class="phone">
+                +212 644 75 25 82
+            </div>
+            <div class="search-div">
+                <img src=<?php echo URLROOT . "/public/img/search.svg" ?> alt="">
+                <input class="search-input" type="text" name="search" id="">
+            </div>
+        </div>
     </nav>
     <div class="sign-in">
         <button class="sign-in-btn">SIGN IN</button>
