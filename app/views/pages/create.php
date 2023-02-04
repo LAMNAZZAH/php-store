603096e2575a2e7f22dfbbc461c637c030bd7b5a
LAMNAZZAH <medlmz01@gmail.com>
2023-02-04 11:18:44 +0100

    minor: styling main page

--- app/views/pages/create.php
+++ app/views/pages/create.php
@@ -2,7 +2,16 @@
 <h1>Create a user</h1>
 <br />
 <h2>provided <?php echo ':' . $data['user'] ?></h2>
-<div id="mydiv" style="height:500px;background:purple;"></div>
+<div class="page-title">
+    <h1>Buy Everything You Need Here</h1>
+</div>
+<div id="featured-product">
+</div>
+<div id="mydiv">
+<h1>Featured Product</h1>
+</div>
+
+<div id="div2" style="background:red;height:5000px;position:sticky;z-index:-1"></div>
 
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>    
--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,28 @@
+window.onscroll = function () {
+  let screenHeight = screen.height;
+  let prevScrollPos = window.scrollY;
+  let subnav = document.getElementById("subnavigation");
+  let navbar = document.getElementById("navigationbar");
+  let mydiv = document.getElementById("mydiv");
 
+  if (prevScrollPos < screenHeight) {
+    mydiv.style.width = Math.max(100 - prevScrollPos, 50) + "%";
+    mydiv.style.marginTop =
+      (prevScrollPos < 230
+        ? 33
+        : 33 + 100 - prevScrollPos > -50
+        ? 33 + 100 - prevScrollPos
+        : -67) + "px";
+    mydiv.style.borderRight = prevScrollPos * 2 + "px solid black";
+  } else if (prevScrollPos > screenHeight) {
+    mydiv.style.opacity = 0 + "%";
+  }
 
-window.onscroll = function() {
-    let prevScrollPos = window.scrollY;
-    let subnav = document.getElementById("subnavigation");
-    let navbar = document.getElementById("navigationbar");
-
-    let myDiv = document.getElementById("mydiv");
-    myDiv.style.color = 'blue';
-
-    subnav.style.height = (prevScrollPos < 200 ? 100 : (200+100-prevScrollPos > 8 ? 200+100-prevScrollPos : 0)) + "px";
-    navbar.style.opacity = Math.max(100-(prevScrollPos/10), 50) + "%";
-}
+  subnav.style.height =
+    (prevScrollPos < 200
+      ? 100
+      : 200 + 100 - prevScrollPos > 8
+      ? 200 + 100 - prevScrollPos
+      : 0) + "px";
+  navbar.style.opacity = Math.max(100 - prevScrollPos / 10, 50) + "%";
+};
